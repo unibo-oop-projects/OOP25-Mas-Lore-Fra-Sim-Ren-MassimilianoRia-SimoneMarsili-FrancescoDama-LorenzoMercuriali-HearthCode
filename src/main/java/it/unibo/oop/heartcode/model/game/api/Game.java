761688c7822f7977cb85c4a7cf7347351ac34cad
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2026-02-20 10:55:08 +0100

    Added temporary Game interface.

---
+++ src/main/java/it/unibo/oop/heartcode/model/game/api/Game.java
@@ -0,0 +1,23 @@
+package it.unibo.oop.heartcode.model.game.api;
+
+/**
+ * The entry point for the model.
+ */
+public interface Game {
+
+    /**
+     * Temp method.
+     */
+    void startGame();
+
+    /**
+     * Temp method.
+     */
+    boolean isOver();
+
+    /**
+     * Temp method.
+     */
+    boolean hasPlayerWon();
+
+}
