bc85ac3b8f5105c7d8cde72c4044b269d753cca6
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2026-02-20 11:45:18 +0100

    Added Creature interface extends Card.

---
+++ src/main/java/it/unibo/oop/heartcode/model/deck/api/Creature.java
@@ -0,0 +1,26 @@
+package it.unibo.oop.heartcode.model.deck.api;
+
+/**
+ * A specific type of card that can be deployed during the match.
+ */
+public interface Creature extends Card {
+
+    /**
+     * @return the health of the creature
+     */
+    Integer getHealth();
+
+    /**
+     * @return the value attack of the creature
+     */
+    Integer getAttackValue();
+
+    /**
+     * Decrease the creature's health.
+     * 
+     * @param damage the amount of damage inflicted
+     * @return the updated value of the creature's health
+     */
+    Integer decreaseHealth(Integer damage);
+
+}
