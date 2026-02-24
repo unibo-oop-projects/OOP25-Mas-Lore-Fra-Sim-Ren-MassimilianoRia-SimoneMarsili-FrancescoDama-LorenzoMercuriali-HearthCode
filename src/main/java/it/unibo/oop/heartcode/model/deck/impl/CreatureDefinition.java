5e0b6f87aa4e5cc6df00b5a2ec8b2d9e9a1f0a16
Simone Marsili <simone.marsili@studio.unibo.it>
2026-02-24 18:09:13 +0100

    Added record CreatureDefinition, in order to deal with a set of cards

---
+++ src/main/java/it/unibo/oop/heartcode/model/deck/impl/CreatureDefinition.java
@@ -0,0 +1,12 @@
+package it.unibo.oop.heartcode.model.deck.impl;
+
+/**
+ * It represents the concept of Creature Definition.
+ * It's basicly a static creature card used in the database, without an associated id.
+ * 
+ * @param name the name of the creature
+ * @param health the amount of health of the creature
+ * @param attackValue the amount of damage the creature can deal
+ * @param manaCost the amount of Mana required to play the card
+ */
+public record CreatureDefinition(String name, Integer health, Integer attackValue, Integer manaCost) { }
