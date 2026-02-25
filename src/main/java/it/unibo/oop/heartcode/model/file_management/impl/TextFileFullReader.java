8afbf3f09fb70388196e47c99f753bd87d27c151
Simone Marsili <simone.marsili@studio.unibo.it>
2026-02-25 11:04:17 +0100

    Modified TestTextFileFullReader in order to correctly load resources frome the classpath. Created FileReader test class

--- src/main/java/it/unibo/oop/heartcode/model/file_management/impl/TextFileFullReader.java
+++ src/main/java/it/unibo/oop/heartcode/model/file_management/impl/TextFileFullReader.java
@@ -2,29 +2,31 @@ package it.unibo.oop.heartcode.model.file_management.impl;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
 import it.unibo.oop.heartcode.model.file_management.api.FullReader;
 
 /**
- * An implementation of {@link FullReader} interface that reads all the lines from a text file.
+ * An implementation of {@link FullReader} interface that reads all the lines from a text file, given a stream.
  */
 public class TextFileFullReader implements FullReader<String> {
 
     private static final Logger LOG = Logger.getLogger(TextFileFullReader.class.getName());
-    private final Path path;
+    private final InputStream stream;
 
     /**
      * Constructs a new TextFileFullReader.
      * 
-     * @param path the path of the file to be read
+     * @param stream the stream to read from
      */
-    public TextFileFullReader(final Path path) {
-        this.path = path;
+    public TextFileFullReader(final InputStream stream) {
+        this.stream = stream;
     }
 
     /**
@@ -32,8 +34,10 @@ public class TextFileFullReader implements FullReader<String> {
      */
     @Override
     public Optional<List<String>> readAll() {
-        try {
-            final List<String> lines = Files.readAllLines(this.path);
+        try (BufferedReader reader = 
+                new BufferedReader(new InputStreamReader(this.stream, StandardCharsets.UTF_8));
+        ) {
+            final List<String> lines = reader.lines().toList();
             return lines.isEmpty() ? Optional.empty() : Optional.of(lines);
         } catch (final IOException e) {
             LOG.log(Level.SEVERE, "Error while reading from file", e);
