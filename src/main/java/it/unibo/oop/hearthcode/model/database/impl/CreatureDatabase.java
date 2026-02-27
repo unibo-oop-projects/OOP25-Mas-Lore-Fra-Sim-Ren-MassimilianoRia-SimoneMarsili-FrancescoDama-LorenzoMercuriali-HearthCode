671943c30528d70630f8cba5ffab45060a417512
Simone Marsili <simone.marsili@studio.unibo.it>
2026-02-27 20:08:26 +0100

    Added a Factory utility for CreatureDatabase

--- src/main/java/it/unibo/oop/hearthcode/model/database/impl/CreatureDatabase.java
+++ src/main/java/it/unibo/oop/hearthcode/model/database/impl/CreatureDatabase.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import it.unibo.oop.hearthcode.model.database.api.Database;
 import it.unibo.oop.hearthcode.model.deck.impl.CreatureDefinition;
-import it.unibo.oop.hearthcode.model.deck.impl.CreatureDefinitionParser;
-import it.unibo.oop.hearthcode.model.file_management.impl.TextFileFullReader;
 
 /**
  * A specific implementation of {@link Database} that contains {@link CreatureDefinition}.
@@ -14,30 +12,13 @@ public final class CreatureDatabase implements Database<CreatureDefinition> {
 
     private final List<CreatureDefinition> definitions;
 
-    private CreatureDatabase(final List<CreatureDefinition> definitions) {
-        this.definitions = definitions;
-    }
-
     /**
-     * Static method for creating an entire database from a file.
+     * Constructs a new creature database from a collection.
      * 
-     * @param fileName the name of the file containing the entities of this database
-     * @return the new Database created
+     * @param definitions the list of {@link CreatureDefinition} used to create the database
      */
-    public static CreatureDatabase createFromFile(final String fileName) {
-        final var stream = ClassLoader.getSystemResourceAsStream(fileName);
-        if (stream == null) {
-            throw new IllegalArgumentException("The resource " + fileName + "couldn't be found.");
-        }
-        final var allLines = new TextFileFullReader(stream).readAll();
-        if (allLines.isEmpty()) {
-            throw new IllegalArgumentException("The resource has no content: can't create empty database");
-        }
-        final var parser = new CreatureDefinitionParser();
-        return new CreatureDatabase(allLines.get().stream()
-            .map(parser::parseLine)
-            .toList()
-        );
+    CreatureDatabase(final List<CreatureDefinition> definitions) {
+        this.definitions = definitions;
     }
 
     @Override
