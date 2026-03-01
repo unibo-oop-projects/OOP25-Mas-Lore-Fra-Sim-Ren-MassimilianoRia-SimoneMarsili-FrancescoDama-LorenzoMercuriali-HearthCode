08977957c430279f48a6ae75f8a341ed0de70b32
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2026-03-01 20:54:45 +0100

    Fixed main class of the application in build.gradle.kts into "it.unibo.oop.hearthcode.App".

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("it.unibo.sampleapp.RateAMovie")
+    mainClass.set("it.unibo.oop.hearthcode.App")
 }
 
 tasks.withType<Test>().configureEach {
