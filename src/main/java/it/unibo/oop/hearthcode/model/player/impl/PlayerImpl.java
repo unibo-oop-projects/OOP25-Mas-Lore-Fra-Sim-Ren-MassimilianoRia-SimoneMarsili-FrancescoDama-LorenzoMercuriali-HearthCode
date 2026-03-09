93055978a7ae1ec862b6e4b6cb6212a2725eb1bc
francesco dama <francesco.dama@studio.unibo.it>
2026-03-09 11:24:01 +0100

    implemented Player interface

--- src/main/java/it/unibo/oop/hearthcode/model/player/impl/PlayerImpl.java
+++ src/main/java/it/unibo/oop/hearthcode/model/player/impl/PlayerImpl.java
@@ -1,8 +1,88 @@
 package it.unibo.oop.hearthcode.model.player.impl;
 
+import it.unibo.oop.hearthcode.model.creature.api.Card;
+import it.unibo.oop.hearthcode.model.creature.api.CardId;
+import it.unibo.oop.hearthcode.model.deck.api.Deck;
+import it.unibo.oop.hearthcode.model.hand.api.Hand;
+import it.unibo.oop.hearthcode.model.hand.impl.HandImpl;
+import it.unibo.oop.hearthcode.model.player.api.Player;
+
 /**
  * implementation of Player.
  */
-public class PlayerImpl {
+public class PlayerImpl implements Player {
 
+    private final ManaManager manaManager;
+    private final Hand hand;
+    private final Deck deck;
+    private int health;
+
+    /**
+     * It constructs a new player.
+     * 
+     * @param deck the deck to be used
+     * @param health the health of the player
+     */
+    public PlayerImpl(final Deck deck, final int health) {
+        this.manaManager = new ManaManager();
+        this.hand = new HandImpl();
+        this.deck = deck;
+        this.health = health;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getActualMana() {
+        return this.manaManager.actualMana();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getTurnManaLimit() {
+        return this.manaManager.maxMana();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void decreaseHealth(final int amount) {
+        this.health -= amount;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void incrementMana() {
+        this.manaManager.updateMana();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Card playCard(final CardId cardId) {
+        return this.hand.removeCard(cardId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void drawCard() {
+        this.hand.addCard(this.deck.draw());
+    }
 }
