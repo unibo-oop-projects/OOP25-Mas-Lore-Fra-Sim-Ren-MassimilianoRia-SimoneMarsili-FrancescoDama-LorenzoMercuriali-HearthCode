84a6953e0f0a1cf1fb44699b58a5001c4884d8d1
francesco dama <francesco.dama@studio.unibo.it>
2026-03-09 09:44:24 +0100

    created an implementation for Hand interface

---
+++ src/main/java/it/unibo/oop/hearthcode/model/hand/impl/HandImpl.java
@@ -0,0 +1,55 @@
+package it.unibo.oop.hearthcode.model.hand.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import it.unibo.oop.hearthcode.model.creature.api.Card;
+import it.unibo.oop.hearthcode.model.creature.api.CardId;
+import it.unibo.oop.hearthcode.model.hand.api.Hand;
+
+/**
+ * Implementation of Hand's interface.
+ */
+public class HandImpl implements Hand {
+
+    private final List<Card> hand;
+
+    /**
+     * simple constructor.
+     */
+    public HandImpl() {
+        this.hand = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getSize() {
+        return this.hand.size();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addCard(final Card card) {
+        this.hand.add(card);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Card removeCard(final CardId cardId) {
+        final Optional<Card> card = this.hand.stream()
+            .filter(c -> c.getId().equals(cardId))
+            .findFirst();
+        if (card.isEmpty()) {
+            throw new IllegalArgumentException("this card isn't contained in your hand");
+        } else {
+            return card.get();
+        }
+    }
+}
