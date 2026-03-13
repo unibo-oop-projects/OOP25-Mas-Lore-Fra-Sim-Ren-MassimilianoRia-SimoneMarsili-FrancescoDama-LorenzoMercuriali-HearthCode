1739032eaa063cd5a7adbb1d91e7c81bb6d19193
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2026-03-13 15:15:34 +0100

    Build style fixes.

--- src/main/java/it/unibo/oop/hearthcode/model/player/api/PlayerType.java
+++ src/main/java/it/unibo/oop/hearthcode/model/player/api/PlayerType.java
@@ -3,9 +3,10 @@ package it.unibo.oop.hearthcode.model.player.api;
 /**
  * A simple enum used to distinguish the two types of player.
  */
-public enum PlayerType {    
+public enum PlayerType {
+
     HUMAN_PLAYER,
 
-    IA_PLAYER;
+    IA_PLAYER
 
 }
