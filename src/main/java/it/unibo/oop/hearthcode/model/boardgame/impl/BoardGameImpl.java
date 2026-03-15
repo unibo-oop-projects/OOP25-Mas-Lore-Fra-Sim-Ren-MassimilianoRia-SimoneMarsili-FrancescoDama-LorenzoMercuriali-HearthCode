60ebc804f868be1b315d85fcee2c58fa32d4a68a
Lorenzo Mercuriali <lorenzo.mercuriali2@studio.unibo.it>
2026-03-15 12:21:56 +0100

    Created BoardGameImpl and partial initgame()

---
+++ src/main/java/it/unibo/oop/hearthcode/model/boardgame/impl/BoardGameImpl.java
@@ -0,0 +1,89 @@
+package it.unibo.oop.hearthcode.model.boardgame.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import it.unibo.oop.hearthcode.model.army.api.Army;
+import it.unibo.oop.hearthcode.model.boardgame.api.BoardGame;
+import it.unibo.oop.hearthcode.model.creature.api.CardId;
+import it.unibo.oop.hearthcode.model.creature.impl.CreatureImplFactory;
+import it.unibo.oop.hearthcode.model.database.impl.CreatureDatabaseFactory;
+import it.unibo.oop.hearthcode.model.deck.api.Deck;
+import it.unibo.oop.hearthcode.model.deck.impl.DeckFactory;
+import it.unibo.oop.hearthcode.model.player.api.Player;
+import it.unibo.oop.hearthcode.model.player.api.PlayerId;
+import it.unibo.oop.hearthcode.model.player.api.PlayerType;
+import it.unibo.oop.hearthcode.model.player.impl.PlayerFactory;
+
+public class BoardGameImpl implements BoardGame{
+
+    private static final String DEFAULT_CREATURES_FILE = "creatures.txt";
+    private static final int DECK_SIZE = 20;
+    private static final int DEFAULT_HEALTH = 30;
+    private final Map<Player, Army> gameState;
+
+    public BoardGameImpl() {
+        this.gameState = new HashMap<>();
+        initGame();
+    }
+
+    private void initGame() {
+        final var generator = new IdGenerator();
+        final var database = CreatureDatabaseFactory.createFromFile(DEFAULT_CREATURES_FILE);
+        final var creatureFactory = new CreatureImplFactory(generator);
+        final var deckFactory = new DeckFactory(database, creatureFactory);
+
+        final Player humanPlayer = PlayerFactory.createPlayer(
+            deckFactory.createWeighted(DECK_SIZE, def -> Math.max(1, def.manaCost())),
+            DEFAULT_HEALTH,
+            new PlayerId(PlayerType.HUMAN_PLAYER)
+        );
+
+        final Player iaPlayer = PlayerFactory.createPlayer(
+            deckFactory.createWeighted(DECK_SIZE, def -> Math.max(1, def.manaCost())),
+            DEFAULT_HEALTH,
+            new PlayerId(PlayerType.IA_PLAYER)
+        );
+        
+        // manca inizializzazione turni ed army
+    }
+
+    @Override
+    public boolean isOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOver'");
+    }
+
+    @Override
+    public Optional<PlayerId> getWinner() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWinner'");
+    }
+
+    @Override
+    public void attackCard(Optional<CardId> attackingIdCard, Optional<CardId> defendingIdCard) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'attackCard'");
+    }
+
+    @Override
+    public void attackHero(Optional<CardId> attackingIdCard, Optional<PlayerId> defendingPlayer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'attackHero'");
+    }
+
+    @Override
+    public void place(Optional<CardId> selectedIdCard, Optional<PlayerId> placingPlayer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'place'");
+    }
+
+    @Override
+    public void switchTurn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'switchTurn'");
+    }
+    
+}
