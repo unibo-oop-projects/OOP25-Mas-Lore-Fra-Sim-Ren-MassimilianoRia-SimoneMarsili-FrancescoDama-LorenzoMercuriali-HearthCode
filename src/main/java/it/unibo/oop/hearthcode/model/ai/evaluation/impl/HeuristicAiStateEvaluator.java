a08fe9081fd1aaffe61cda4b6ce2d2e58ea7ce1a
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2026-04-24 10:21:36 +0200

    Restructured ai evalution folder.

---
+++ src/main/java/it/unibo/oop/hearthcode/model/ai/evaluation/impl/HeuristicAiStateEvaluator.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.hearthcode.model.ai.evaluation.impl;
+
+import it.unibo.oop.hearthcode.model.ai.evaluation.api.AiStateEvaluator;
+import it.unibo.oop.hearthcode.model.ai.simulation.api.AiGameState;
+
+/**
+ * Heuristic evaluator for AI simulated states.
+ */
+public final class HeuristicAiStateEvaluator implements AiStateEvaluator {
+
+    @Override
+    public int evaluate(AiGameState gameState) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'evaluate'");
+    }
+    
+}
