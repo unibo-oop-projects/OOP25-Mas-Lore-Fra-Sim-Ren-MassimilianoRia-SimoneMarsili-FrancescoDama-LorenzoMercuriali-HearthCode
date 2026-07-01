e9e7ff531781464576096507873da7a9d5edeb13
Danilo Pianini <danilo.pianini@unibo.it>
2026-07-01 11:34:02 +0200

    ci: add the OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,16 @@
+rootProject.name = "oop-25-MassimilianoRia-SimoneMarsili-FrancescoDama-LorenzoMercuriali-HearthCode"
 plugins {
-    // Automatically downloads the correct java version to run the static analyzers
+    id("com.gradle.develocity") version "4.5.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "HearthCode"
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        buildScanPublished {
+            file("scan-journal.log").writeText("$buildScanId - $buildScanUri\n")
+        }
+    }
+}
